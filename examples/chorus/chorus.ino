7777918a83e14422a7dd9a6cda297dbac99bc84e
Clemens @ Elektor <60133590+ClemensAtElektor@users.noreply.github.com>
2024-07-19 08:53:05 +0200

    Update chorus.ino
    Corrected typo

--- examples/chorus/chorus.ino
+++ examples/chorus/chorus.ino
@@ -12,7 +12,7 @@
  * - Set JP1 ('DSP clock') to position 'X1' (pins 1 & 2)
  * - Set JP2 to position 'Selfboot' (pins 2 & 3)
  * - Use P5 to adjust TP1 to 3.15 V
- * - Upload this sketch to ESP32
+ * - Upload this sketch to the ESP32
  * - Wait for LED1 to start blinking at a rate of 1 Hz
  * - Connect sound source to K2
  * - Connect headphones or amplifier to K3 and enjoy
